31620e6fc267bcdfb1b355b36ee42e1b59093d96
TimmyNic <tim.nicolas@outlook.com>
2023-03-02 15:14:52 +1300

    Zinc view widget extends BaseSceneviewerWidget
    The ZincPointCloudPartitionerWidget now inherits from BaseSceneviewerWidget rather than SceneviewerWidget. This gives us an interface to register a ZincWidgets handler to automatically handle mouse events.

--- mapclientplugins/pointcloudpartitionerstep/view/pointcloudpartitionerwidget.py
+++ mapclientplugins/pointcloudpartitionerstep/view/pointcloudpartitionerwidget.py
@@ -5,6 +5,8 @@ Created on Jun 18, 2015
 """
 from PySide6 import QtGui, QtWidgets
 
+from opencmiss.zincwidgets.handlers.scenemanipulation import SceneManipulation
+
 from mapclientplugins.pointcloudpartitionerstep.view.ui_pointcloudpartitionerwidget import Ui_PointCloudPartitionerWidget
 from mapclientplugins.pointcloudpartitionerstep.scene.pointcloudpartitionerscene import PointCloudPartitionerScene
 
@@ -29,7 +31,9 @@ class PointCloudPartitionerWidget(QtWidgets.QWidget):
         self._model = model
         self._scene = PointCloudPartitionerScene(model)
 
-        self._ui.widgetZinc.setContext(model.getContext())
+        self._ui.widgetZinc.set_context(model.getContext())
+        self._ui.widgetZinc.register_handler(SceneManipulation())
+
         # self._ui.widgetZinc.setSelectionfilter(model.getSelectionfilter())
 
         self._makeConnections()
@@ -37,7 +41,7 @@ class PointCloudPartitionerWidget(QtWidgets.QWidget):
     def _makeConnections(self):
         self._ui.pushButtonContinue.clicked.connect(self._continueExecution)
         self._ui.pushButtonViewAll.clicked.connect(self._viewAllButtonClicked)
-        self._ui.widgetZinc.graphicsInitialized.connect(self._zincWidgetReady)
+        self._ui.widgetZinc.graphics_initialized.connect(self._zincWidgetReady)
         self._ui.pushButtonDeleteNode.clicked.connect(self._ui.widgetZinc.deleteSelectedNodes)
 
     def getLandmarks(self):
@@ -50,7 +54,7 @@ class PointCloudPartitionerWidget(QtWidgets.QWidget):
         self._callback = done_exectution
 
     def _zincWidgetReady(self):
-        self._ui.widgetZinc.setSelectionfilter(self._model.getSelectionfilter())
+        self._ui.widgetZinc.set_selectionfilter(self._model.getSelectionfilter())
 
     def _viewAllButtonClicked(self):
         self._ui.widgetZinc.viewAll()
