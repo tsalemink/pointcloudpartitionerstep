c27c628cef014fc3b1ad509a7767f870ac98e0a9
Hugh Sorby <h.sorby@auckland.ac.nz>
2023-11-08 16:22:23 +1300

    Set plugin location.

--- mapclientplugins/pointcloudpartitionerstep/__init__.py
+++ mapclientplugins/pointcloudpartitionerstep/__init__.py
@@ -5,7 +5,7 @@ MAP Client Plugin
 __version__ = '0.2.0'
 __author__ = 'Timothy Salemink'
 __stepname__ = 'Point Cloud Partitioner'
-__location__ = ''
+__location__ = 'https://github.com/mapclient-plugins/mapclientplugins.pointcloudpartitionerstep'
 
 # import class that derives itself from the step mountpoint.
 from mapclientplugins.pointcloudpartitionerstep import step
